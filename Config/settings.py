96ebe65acf5a56bcde2b0ece0889c3e1b74eed9e
Yodgorbek <yodgorbekaxmedov0@gmail.com>
2025-02-06 00:22:58 +0500

    allowed host

--- Config/settings.py
+++ Config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["64.226.118.36"]
 
 # Application definition
 
